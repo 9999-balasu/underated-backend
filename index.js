361dab1cbc0fa23c26dd3c21616edec434d0888a
9999-balasu <138554962+9999-balasu@users.noreply.github.com>
2025-04-13 13:19:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express') ;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(req,res) =>{
+    res.send('Hello, Express Backend')
+})
+
+
+
+
+
+const PORT = process.env.PORT || 5000  
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
